41a677c6fed8b39d6626855565a1950d588d1259
Ruchi Patel <rucipatel9@gmail.com>
2024-12-15 18:37:10 -0500

    Update sayHello.groovy

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
-def call(String name, String dayOfWeek) {
-   echo "Hello, ${name}. Today is $ {dayOfWeek}"
+def call(String name,String dayOfWeek) {
+   echo "Hello, ${name}. Today is ${dayOfWeek}"
 }
